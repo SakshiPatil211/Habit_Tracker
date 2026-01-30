064fe19f98910ec5290ab5aed40d0ddf8131f6cd
GautamiSanap21 <147413892+GautamiSanap21@users.noreply.github.com>
2026-01-30 10:39:44 +0530

    Delete DTOs directory

--- DTOs/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HabitTracker.DTOs
-{
-    public class LoginDto
-    {
-        public required string UsernameOrEmailOrMobile { get; set; }
-        public required string Password { get; set; }
-
-    }
-}
