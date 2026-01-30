2077b963d60c331c47112ec8b31ae09bbe60dc4a
GautamiSanap21 <sanapgautami2107@gmail.com>
2026-01-30 12:34:00 +0530

    Habit Tracker Backend

---
+++ HabitTrackerBackend/DTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HabitTracker.DTOs
+{
+    public class LoginDto
+    {
+        public required string UsernameOrEmailOrMobile { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
