1e1271cae6953909fae29eb2056bb76306819de4
SakshiPatil211 <sakshivpatil211@gmail.com>
2026-01-31 11:51:38 +0530

    habit_tracker_bakend

---
+++ HabitTrackerBackendsakshi/Models/Classes/HabitSchedule.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Habit_Tracker_Backend.Models.Classes
+{
+    [Table("habit_schedule")]
+    public class HabitSchedule
+    {
+        [Key]
+        [Column("schedule_id")]
+        public long ScheduleId { get; set; }
+
+        [Column("habit_id")]
+        public long HabitId { get; set; }
+
+        [Column("day_of_week")]
+        public string DayOfWeek { get; set; } = null!;
+
+        // 🔗 Navigation Property
+        public Habit Habit { get; set; } = null!;
+    }
+}
