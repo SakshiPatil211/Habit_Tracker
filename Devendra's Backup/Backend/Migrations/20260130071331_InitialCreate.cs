20124095fd9f18b32c727e0380695883a2ac77ed
Devendra-cdx <singh466dev@gmail.com>
2026-01-31 11:44:44 +0530

    add Migration in backend

---
+++ Devendra's Backup/Backend/Migrations/20260130071331_InitialCreate.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Habit_Tracker_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "feedback",
+                columns: table => new
+                {
+                    feedback_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    rating = table.Column<int>(type: "int", nullable: false),
+                    message = table.Column<string>(type: "varchar(2000)", maxLength: 2000, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_feedback", x => x.feedback_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "habit_categories",
+                columns: table => new
+                {
+                    category_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    category_name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    description = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    is_active = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_habit_categories", x => x.category_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    first_name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    middle_name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    last_name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    username = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    mobile_number = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password_hash = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    dob = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    is_active = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    is_mobile_verified = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    is_email_verified = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    role = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    email_notifications_enabled = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "habits",
+                columns: table => new
+                {
+                    habit_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    user_id = table.Column<long>(type: "bigint", nullable: false),
+                    category_id = table.Column<long>(type: "bigint", nullable: false),
+                    habit_name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    description = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    start_date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    end_date = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    priority = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    is_active = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_habits", x => x.habit_id);
+                    table.ForeignKey(
+                        name: "FK_habits_habit_categories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "habit_categories",
+                        principalColumn: "category_id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_habits_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "user_otp",
+                columns: table => new
+                {
+                    otp_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    user_id = table.Column<long>(type: "bigint", nullable: false),
+                    otp_code_hash = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    otp_type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    channel = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    expires_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    is_used = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    attempts = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_otp", x => x.otp_id);
+                    table.ForeignKey(
+                        name: "FK_user_otp_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "habit_log",
+                columns: table => new
+                {
+                    log_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    habit_id = table.Column<long>(type: "bigint", nullable: false),
+                    log_date = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    status = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    remarks = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_habit_log", x => x.log_id);
+                    table.ForeignKey(
+                        name: "FK_habit_log_habits_habit_id",
+                        column: x => x.habit_id,
+                        principalTable: "habits",
+                        principalColumn: "habit_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "habit_reminder",
+                columns: table => new
+                {
+                    reminder_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    habit_id = table.Column<long>(type: "bigint", nullable: false),
+                    reminder_time = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    is_enabled = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_habit_reminder", x => x.reminder_id);
+                    table.ForeignKey(
+                        name: "FK_habit_reminder_habits_habit_id",
+                        column: x => x.habit_id,
+                        principalTable: "habits",
+                        principalColumn: "habit_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "habit_schedule",
+                columns: table => new
+                {
+                    schedule_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    habit_id = table.Column<long>(type: "bigint", nullable: false),
+                    day_of_week = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_habit_schedule", x => x.schedule_id);
+                    table.ForeignKey(
+                        name: "FK_habit_schedule_habits_habit_id",
+                        column: x => x.habit_id,
+                        principalTable: "habits",
+                        principalColumn: "habit_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "habit_streaks",
+                columns: table => new
+                {
+                    streak_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    habit_id = table.Column<long>(type: "bigint", nullable: false),
+                    current_streak = table.Column<int>(type: "int", nullable: false),
+                    longest_streak = table.Column<int>(type: "int", nullable: false),
+                    last_completed_date = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_habit_streaks", x => x.streak_id);
+                    table.ForeignKey(
+                        name: "FK_habit_streaks_habits_habit_id",
+                        column: x => x.habit_id,
+                        principalTable: "habits",
+                        principalColumn: "habit_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_habit_log_habit_id_log_date",
+                table: "habit_log",
+                columns: new[] { "habit_id", "log_date" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_habit_reminder_habit_id",
+                table: "habit_reminder",
+                column: "habit_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_habit_schedule_habit_id_day_of_week",
+                table: "habit_schedule",
+                columns: new[] { "habit_id", "day_of_week" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_habit_streaks_habit_id",
+                table: "habit_streaks",
+                column: "habit_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_habits_category_id",
+                table: "habits",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_habits_user_id_habit_name",
+                table: "habits",
+                columns: new[] { "user_id", "habit_name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_otp_expires_at",
+                table: "user_otp",
+                column: "expires_at");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_otp_user_id",
+                table: "user_otp",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_username",
+                table: "users",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "feedback");
+
+            migrationBuilder.DropTable(
+                name: "habit_log");
+
+            migrationBuilder.DropTable(
+                name: "habit_reminder");
+
+            migrationBuilder.DropTable(
+                name: "habit_schedule");
+
+            migrationBuilder.DropTable(
+                name: "habit_streaks");
+
+            migrationBuilder.DropTable(
+                name: "user_otp");
+
+            migrationBuilder.DropTable(
+                name: "habits");
+
+            migrationBuilder.DropTable(
+                name: "habit_categories");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
